37dffa21ccf6686b736be2d878f30851e638c691
Andilun <Anders-lunde@hotmail.com>
2022-07-09 14:25:46 +0200

    fixed formatting

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/TimeSlot.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/TimeSlot.java
@@ -12,67 +12,67 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name="time_slots")
+@Table(name = "time_slots")
 public class TimeSlot {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "time_slot_id")
-    private Long timeSlotId;
-    
-    @Column(name = "time_slot_date")
-    private LocalDate timeSlotDate;
-    
-    @Column(name = "start_time")
-    private LocalDateTime startTime;
-    
-    @Column(name = "end_time")
-    private LocalDateTime endTime;
-    
-    @Column(name = "is_keynote_time_slot")
-    private boolean isKeynoteTimeSlot;
-    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "time_slot_id")
+	private Long timeSlotId;
+
+	@Column(name = "time_slot_date")
+	private LocalDate timeSlotDate;
+
+	@Column(name = "start_time")
+	private LocalDateTime startTime;
+
+	@Column(name = "end_time")
+	private LocalDateTime endTime;
+
+	@Column(name = "is_keynote_time_slot")
+	private boolean isKeynoteTimeSlot;
+
 	public TimeSlot() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public Long getTimeSlotId() {
-		return timeSlotId;
-	}
-
-	public void setTimeSlotId(Long timeSlotId) {
-		this.timeSlotId = timeSlotId;
-	}
-
-	public LocalDate getTimeSlotDate() {
-		return timeSlotDate;
-	}
-
-	public void setTimeSlotDate(LocalDate timeSlotDate) {
-		this.timeSlotDate = timeSlotDate;
-	}
-
-	public LocalDateTime getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(LocalDateTime startTime) {
-		this.startTime = startTime;
 	}
 
 	public LocalDateTime getEndTime() {
 		return endTime;
 	}
 
-	public void setEndTime(LocalDateTime endTime) {
-		this.endTime = endTime;
-	}
-
 	public Boolean getIsKeynoteTimeSlot() {
 		return isKeynoteTimeSlot;
 	}
 
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public LocalDate getTimeSlotDate() {
+		return timeSlotDate;
+	}
+
+	public Long getTimeSlotId() {
+		return timeSlotId;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
 	public void setIsKeynoteTimeSlot(Boolean isKeynoteTimeSlot) {
 		this.isKeynoteTimeSlot = isKeynoteTimeSlot;
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public void setTimeSlotDate(LocalDate timeSlotDate) {
+		this.timeSlotDate = timeSlotDate;
+	}
+
+	public void setTimeSlotId(Long timeSlotId) {
+		this.timeSlotId = timeSlotId;
 	}
 
 	@Override
@@ -80,5 +80,5 @@ public class TimeSlot {
 		return "TimeSlot [timeSlotId=" + timeSlotId + ", timeSlotDate=" + timeSlotDate + ", startTime=" + startTime
 				+ ", endTime=" + endTime + ", isKeynoteTimeSlot=" + isKeynoteTimeSlot + "]";
 	}
-	
+
 }
